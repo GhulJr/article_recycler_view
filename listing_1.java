e0c045cbf474f24b911e36e6c7d5af50148a5cc5
GhulJr <51434437+GhulJr@users.noreply.github.com>
2022-03-30 18:54:00 +0200

    Create listing_1.java

---
+++ listing_1.java
@@ -0,0 +1,148 @@
+@Nullable
+ViewHolder tryGetViewHolderForPositionByDeadline(int position,boolean dryRun, long deadlineNs) {
+    if (position < 0 || position >= mState.getItemCount()) {
+        throw new IndexOutOfBoundsException("Invalid item position " + position
+                + "(" + position + "). Item count:" + mState.getItemCount()
+                + exceptionLabel());
+    }
+    boolean fromScrapOrHiddenOrCache = false;
+    ViewHolder holder = null;
+    if (mState.isPreLayout()) {
+        holder = getChangedScrapViewForPosition(position);
+        fromScrapOrHiddenOrCache = holder != null;
+    }
+    if (holder == null) {
+				// BANANA 1 - first try to obtain the view from first-level cache
+        holder = getScrapOrHiddenOrCachedHolderForPosition(position, dryRun);
+        if (holder != null) {
+            if (!validateViewHolderForOffsetPosition(holder)) {
+                if (!dryRun) {
+                    holder.addFlags(ViewHolder.FLAG_INVALID);
+                    if (holder.isScrap()) {
+                        removeDetachedView(holder.itemView, false);
+                        holder.unScrap();
+                    } else if (holder.wasReturnedFromScrap()) {
+                        holder.clearReturnedFromScrapFlag();
+                    }
+                    recycleViewHolderInternal(holder);
+                }
+                holder = null;
+            } else {
+                fromScrapOrHiddenOrCache = true;
+            }
+        }
+    }
+    if (holder == null) {
+        final int offsetPosition = mAdapterHelper.findPositionOffset(position);
+        if (offsetPosition < 0 || offsetPosition >= mAdapter.getItemCount()) {
+            throw new IndexOutOfBoundsException("Inconsistency detected. Invalid item "
+                    + "position " + position + "(offset:" + offsetPosition + ")."
+                    + "state:" + mState.getItemCount() + exceptionLabel());
+        }
+
+        final int type = mAdapter.getItemViewType(offsetPosition);
+        if (mAdapter.hasStableIds()) {
+						// BANANA 2 - second try to obtain the view from first-level cache
+            holder = getScrapOrCachedViewForId(mAdapter.getItemId(offsetPosition),
+                type, dryRun);
+            if (holder != null) {
+                // update position
+                holder.mPosition = offsetPosition;
+                fromScrapOrHiddenOrCache = true;
+            }
+        }
+        if (holder == null && mViewCacheExtension != null) {
+            final View view = mViewCacheExtension
+                .getViewForPositionAndType(this, position, type);
+            if (view != null) {
+                holder = getChildViewHolder(view);
+                if (holder == null) {
+                    throw new IllegalArgumentException("getViewForPositionAndType returned"
+                            + " a view which does not have a ViewHolder"
+                            + exceptionLabel());
+                } else if (holder.shouldIgnore()) {
+                    throw new IllegalArgumentException("getViewForPositionAndType returned"
+                            + " a view that is ignored. You must call stopIgnoring before"
+                            + " returning this view." + exceptionLabel());
+                }
+            }
+        }
+        if (holder == null) { 
+            if (DEBUG) {
+                Log.d(TAG, "tryGetViewHolderForPositionByDeadline("
+                        + position + ") fetching from shared pool");
+            }
+						// BANANA 3 - last hope to obtain view, this time from RecycledViewPool
+            holder = getRecycledViewPool().getRecycledView(type);
+            if (holder != null) {
+                holder.resetInternal();
+                if (FORCE_INVALIDATE_DISPLAY_LIST) {
+                    invalidateDisplayListInt(holder);
+                }
+            }
+        }
+        if (holder == null) {
+            long start = getNanoTime();
+            if (deadlineNs != FOREVER_NS
+                && !mRecyclerPool.willCreateInTime(type, start, deadlineNs)) {
+                return null;
+            }
+						// BANANA 4 - new view is created
+            holder = mAdapter.createViewHolder(RecyclerView.this, type);
+            if (ALLOW_THREAD_GAP_WORK) {
+                // only bother finding nested RV if prefetching
+                RecyclerView innerView = findNestedRecyclerView(holder.itemView);
+                if (innerView != null) {
+                    holder.mNestedRecyclerView = new WeakReference<>(innerView);
+                }
+            }
+
+            long end = getNanoTime();
+            mRecyclerPool.factorInCreateTime(type, end - start);
+            if (DEBUG) {
+                Log.d(TAG, "tryGetViewHolderForPositionByDeadline created new ViewHolder");
+            }
+        }
+    }
+
+    if (fromScrapOrHiddenOrCache && !mState.isPreLayout() && holder
+            .hasAnyOfTheFlags(ViewHolder.FLAG_BOUNCED_FROM_HIDDEN_LIST)) {
+        holder.setFlags(0, ViewHolder.FLAG_BOUNCED_FROM_HIDDEN_LIST);
+        if (mState.mRunSimpleAnimations) {
+            int changeFlags = ItemAnimator
+                    .buildAdapterChangeFlagsForAnimations(holder);
+            changeFlags |= ItemAnimator.FLAG_APPEARED_IN_PRE_LAYOUT;
+            final ItemHolderInfo info = mItemAnimator.recordPreLayoutInformation(mState,
+                holder, changeFlags, holder.getUnmodifiedPayloads());
+            recordAnimationInfoIfBouncedHiddenView(holder, info);
+        }
+    }
+
+    boolean bound = false;
+    if (mState.isPreLayout() && holder.isBound()) {
+        holder.mPreLayoutPosition = position;
+    } else if (!holder.isBound() || holder.needsUpdate() || holder.isInvalid()) {
+        if (DEBUG && holder.isRemoved()) {
+            throw new IllegalStateException("Removed holder should be bound and it should"
+                    + " come here only in pre-layout. Holder: " + holder
+                    + exceptionLabel());
+        }
+        final int offsetPosition = mAdapterHelper.findPositionOffset(position);
+        bound = tryBindViewHolderByDeadline(holder, offsetPosition, position, deadlineNs);
+    }
+
+    final ViewGroup.LayoutParams lp = holder.itemView.getLayoutParams();
+    final LayoutParams rvLayoutParams;
+    if (lp == null) {
+        rvLayoutParams = (LayoutParams) generateDefaultLayoutParams();
+        holder.itemView.setLayoutParams(rvLayoutParams);
+    } else if (!checkLayoutParams(lp)) {
+        rvLayoutParams = (LayoutParams) generateLayoutParams(lp);
+        holder.itemView.setLayoutParams(rvLayoutParams);
+    } else {
+        rvLayoutParams = (LayoutParams) lp;
+    }
+    rvLayoutParams.mViewHolder = holder;
+    rvLayoutParams.mPendingInvalidate = fromScrapOrHiddenOrCache && bound;
+    return holder;
+}
